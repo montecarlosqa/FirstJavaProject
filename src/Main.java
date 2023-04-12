7385fc8e7d1f541662babd97e0ee741fa5864713
montecarlosqa <montecarlosqa@gmail.com>
2023-04-12 11:26:40 -0700

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
-
-        System.out.println("Hello and welcome!");
-        System.out.print("Please enter your name: ");
-        String name = in.nextLine();
-
-        System.out.print("Please enter your title: ");
-        String title = in.nextLine();
-
-        System.out.printf("Name: %s \n", name);
-        System.out.printf("Title: %s ", title);
-
+        System.out.println("Hello World!");
 
     }
 }
